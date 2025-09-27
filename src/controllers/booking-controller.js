2dbab5fe7784f97c1e484c74a8b998d0a8991feb
SHIVAM-SHRIVATSAVA-1605 <shivam091204@gmail.com>
2025-09-27 23:25:42 +0530

    Generated controller demo code

---
+++ src/controllers/booking-controller.js
@@ -0,0 +1,15 @@
+const { BookingSrvice } = require('../services/index');BookingSrvice
+
+const bookingService = new BookingService();
+
+const create = async (req, res) => {
+    try {
+        const response = await  
+    } catch(error) {
+
+    }
+}
+
+module.exports = {
+
+}
